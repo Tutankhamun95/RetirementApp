cc1f7e55607a64c90fa650b1ec5a97abb410c587
Tutankhamun95 <ali-mahdy@windowslive.com>
2020-03-19 16:08:18 +0800

    000webhostapp blocked ip, changed network to upload thru ftp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 
 @section('content') 
 
-<svg class="hidden">
+{{-- <svg class="hidden">
   <defs>
     <symbol id="icon-arrow" viewBox="0 0 24 24">
       <title>arrow</title>
@@ -52,10 +52,10 @@
       <path d="M12,18.2c-0.4,0-0.7-0.3-0.7-0.7s0.3-0.7,0.7-0.7c1.3,0,2.4-1.1,2.4-2.4c0-0.4,0.3-0.7,0.7-0.7c0.4,0,0.7,0.3,0.7,0.7C15.8,16.5,14.1,18.2,12,18.2z" />
     </symbol>
   </defs>
-</svg>
+</svg> --}}
 <main>
   <header class="codrops-header">
-    <div class="codrops-links">
+    {{-- <div class="codrops-links">
       <a class="codrops-icon codrops-icon--prev" href="http://tympanus.net/Development/FancyLetterAnimation/" title="Previous Demo">
         <svg class="icon icon--arrow">
           <use xlink:href="#icon-arrow"></use>
@@ -75,14 +75,14 @@
         <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
         <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
       </svg>
-    </a>
+    </a> --}}
   </header>
   <!-- Initial markup -->
-  <div class="segmenter" style="background-image: url('{{asset('assets/frontend/2.jpg')}}');"></div>
+  <div class="segmenter" style="background-image: url('{{asset('assets/frontend/img/2.jpg')}}');"></div>
   <h2 class="trigger-headline trigger-headline--hidden"><span>C</span><span>o</span><span>p</span><span>p</span><span>e</span><span>r</span></h2>
   <div class="bottom-nav">
     <button class="btn btn--trigger">Show Effect</button>
-    <nav class="codrops-demos">
+    {{-- <nav class="codrops-demos">
       <span>Examples: </span>
       <a href="index.html">1</a>
       <a class="current-demo" href="index2.html">2</a>
@@ -90,7 +90,7 @@
       <a href="index4.html">4</a>
       <a href="index5.html">5</a>
       <a href="index6.html">6</a>
-    </nav>
+    </nav> --}}
   </div>
 </main>
 
